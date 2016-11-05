1ef9b5be9ed9463d8776e836f6a86334b7c6ac96
FritzFlorian <codingtipp@gmail.com>
2016-11-06 00:41:33 +0100

    Fix: copy paste error in deployment config

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :gameoff, Gameoff.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, Gameoff.Repo,
+config :gameoff, Gameoff.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
