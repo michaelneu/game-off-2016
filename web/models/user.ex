8028e9c6bd29f039a72c74d6ee9ff8b4f7ae7417
FritzFlorian <codingtipp@gmail.com>
2016-11-20 21:12:10 +0100

    Add position to users model

--- web/models/user.ex
+++ web/models/user.ex
@@ -4,6 +4,8 @@ defmodule Gameoff.User do
   schema "users" do
     field :name, :string
     field :github_uid, :string
+    field :location_x, :integer
+    field :location_y, :integer
 
     has_many :user_devices, Gameoff.UserDevice
     has_many :devices, through: [:user_devices, :device]
@@ -18,7 +20,7 @@ defmodule Gameoff.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:name, :github_uid, :current_repository_id])
+    |> cast(params, [:name, :github_uid, :current_repository_id, :location_x, :location_y])
     |> cast_assoc(:current_repository)
     |> validate_required([:name, :github_uid])
   end
